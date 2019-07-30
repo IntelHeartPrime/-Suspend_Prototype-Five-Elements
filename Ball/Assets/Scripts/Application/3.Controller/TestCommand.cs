80ab3cd1b44a5707096b7fe3fa224914010612d2
intelheart <yanxin_951226@163.com>
2019-07-30 09:03:59 +0800

    update the data

--- Ball/Assets/Scripts/Application/3.Controller/TestCommand.cs
+++ Ball/Assets/Scripts/Application/3.Controller/TestCommand.cs
@@ -6,7 +6,7 @@ public class TestCommand : VisualController
 {
     public override void Execute(object data){
         Debug.Log("Register Manager is sucessful!");
-        VisualMVC.Instance.SendEvent(Consts.C_ChangeScene,"1.Start");
+        //VisualMVC.Instance.SendEvent(Consts.C_ChangeScene,"1.Start");
         DestorySelf();
     }
 
